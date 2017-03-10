fc5fbb0fbd0d63999fdc6441d1f95c18d8341bd1
Steven Jacobs <sjaco002@ucr.edu>
2017-03-10 11:23:15 -0800

    Made range query run for an hour

--- startRangeQuery.sh
+++ startRangeQuery.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
 
-COUNTER=0
-while [  $COUNTER -lt 3600 ]; do
-	curl -G -H "Accept: application/x-adm" -v --data-urlencode 'aql=use dataverse experiments;
-	for $i in dataset Tweets1
-	where  $i.tweet_id > 0
-	and $i.tweet_id < 5000
-	return $i.id
-	' http://localhost:19002/aql > errors.log
-	echo The counter is $COUNTER
-	let COUNTER=COUNTER+1
+end=$((SECONDS+3600))
+while [  $SECONDS -lt $end ]; do
+	seconds=$((SECONDS+1))
+	mx=$((seconds * 9000 - 5000))
+        rx=$(($RANDOM%mx))
+        m=$((rx+5000))
+	curl -G -H "Accept: application/x-adm" -v --data-urlencode "aql=use dataverse experiments; for \$i in dataset Tweets1 where  \$i.tweet_id > $rx and \$i.tweet_id < $m return \$i.id" http://localhost:19002/aql > errors.log
+	echo $rx
 	sleep 1	
 done
 
