c1e3a199f481390804f2bc06029830be61af571e
Steven Jacobs <sjaco002@ucr.edu>
2017-03-29 17:04:10 -0700

    Fixed Random Generator

--- startRangeQuery.sh
+++ startRangeQuery.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
-end=$((SECONDS+34400))
+RANDOM=$$
+end=$((SECONDS+43200))
 while [  $SECONDS -lt $end ]; do
 	seconds=$((SECONDS+1))
-	mx=$((seconds * 9000 - 5000))
-        rx=$(($RANDOM%mx))
+	total=$((seconds * 2000))
+ 	mx=$((total-5000))	
+	rx=$(shuf -i 0-$mx -n 1)
         m=$((rx+5000))
 	curl -G -H "Accept: application/x-adm" -v --data-urlencode "aql=use dataverse experiments; for \$i in dataset Tweets1 where  \$i.tweet_id > $rx and \$i.tweet_id < $m return \$i.id" http://localhost:19002/aql > errors.log
+	echo $seconds
+	echo $mx
 	echo $rx
+	echo $m
 	sleep 1	
 done
 
